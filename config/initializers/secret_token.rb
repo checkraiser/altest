2f19d56b2eb643f245b0ee7a3130391470c206c6
Truong Hoang Dung <revskill09@gmail.com>
2014-03-26 16:38:08 +0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Altest::Application.config.secret_key_base = '0807a713b3249e372e00a83f84e3a998bf1190afa2c1b3986d1e682d2308e13be7dee18fd46b0313e48745d9c9aa9978be695870f6cadc7d017d0085cb77c1f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Altest::Application.config.session_store :cookie_store, key: '_altest_session'
